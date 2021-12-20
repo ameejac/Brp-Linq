616bbc135ec4e746e5b7f23c6ed1e2f0760c5b0f
Ameejac <ameejacherukuru123@gmail.com>
2021-12-20 15:46:05 +0530

    Ameeja[added: num and names by using IEnumerable ]

---
+++ LinqDemo/LinqCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace LinqDemo
+{
+    class LinqCode
+    {
+        public void Inumerablemethod()
+        {
+            int[] data = { 1, 2, 3, 4, 5, 6 };
+            IEnumerable<int> dataquery = from datas in data where datas > 2 select datas;
+            foreach(var items in dataquery)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public void NamesUsingLinQ()
+        {
+            string[] names = { "red", "orange", "blue", "black", "brinjal" };
+            IEnumerable<string> namequery = names.Where(A =>A.Contains ("i"));
+            foreach(var elements in namequery)
+            {
+                Console.WriteLine(elements);
+            }
+         }
+    }
+}
---
+++ LinqDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqCode obj = new LinqCode();
+            obj.Inumerablemethod();
+            obj.NamesUsingLinQ();
+        }
+    }
+}
